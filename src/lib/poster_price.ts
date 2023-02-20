a7cc50d1fba1ac715adeb9cd534325985e44a560
jeffairsonn <thomasbelenuspro@gmail.com>
2023-02-20 18:13:31 +0100

    add live price to production

--- src/lib/poster_price.ts
+++ src/lib/poster_price.ts
@@ -2,19 +2,19 @@ const posters = [
   {
     name: 'Poster 30x30cm',
     price: 19.99,
-    live_price_id: 'price_1Jj9Z2pJZ2Z2Z2Z2Z2Z2Z2Z2Z2',
+    live_price_id: 'price_1MdcfxGTAYNLvctQlUrGzBon',
     price_id: 'price_1Maho3GTAYNLvctQ2FYqZrMC',
   },
   {
     name: 'Poster 50x50cm',
     price: 24.99,
-    live_price_id: 'price_1hJ9Z2pJZ2Z2Z2Z2Z2Z2Z2Z2Z2',
+    live_price_id: 'price_1MdcfxGTAYNLvctQ2OACCkwQ',
     price_id: 'price_1Maho3GTAYNLvctQMTDruOJT',
   },
   {
     name: 'Poster 70x70cm',
     price: 34.99,
-    live_price_id: 'price_1J9Zt2pJZ2Z2Z2Z2Z2Z2Z2Z2Z2',
+    live_price_id: 'price_1MdcfxGTAYNLvctQlUrGzBon',
     price_id: 'price_1Maho3GTAYNLvctQeLl4BGld',
   },
 ];
--- src/pages/api/auth/register.ts
+++ src/pages/api/auth/register.ts
@@ -22,7 +22,6 @@ export default async function handler(
       res.status(200).json(response.data);
     })
     .catch((error) => {
-      console.log('prblm');
       console.log('error', error.response.data);
     });
 
